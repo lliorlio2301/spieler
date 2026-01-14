62df3d5ffe633b838f9b45a33640c65e4298ca6a
lliorlio2301 <jorge2301@outlook.es>
2026-01-14 22:28:01 +0100

    Project Gerüst

---
+++ src/main/java/dhsn/verwaltung/spieler/SpielerApplication.java
@@ -0,0 +1,13 @@
+package dhsn.verwaltung.spieler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpielerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpielerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dhsn/verwaltung/spieler/SpielerApplicationTests.java
@@ -0,0 +1,13 @@
+package dhsn.verwaltung.spieler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpielerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
