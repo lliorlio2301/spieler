110a0d5ba415d7f79bbe80c5df2107b34f066c85
lliorlio2301 <jorge2301@outlook.es>
2026-01-16 16:28:29 +0100

    Erstellung der verschiedenen Rollen

--- src/main/java/dhsn/verwaltung/spieler/model/identity/Benutzer.java
+++ src/main/java/dhsn/verwaltung/spieler/model/identity/Benutzer.java
@@ -1,10 +1,19 @@
 package dhsn.verwaltung.spieler.model.identity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinColumns;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -16,13 +25,18 @@ public class Benutzer {
     private Long id;
 
     @Column(unique = true, nullable = false)
-    private String nickname;
+    private String username;
     
     private String passwort;
 
     private String vorname;
     private String nachname;
 
-
-    
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable( // Spring configures it automatically but it is configurated, sothat the names of the columns doesn't change if the model's variables change
+        name = "benutzer_roles", 
+        joinColumns = @JoinColumn(name = "benutzer_id", referencedColumnName = "id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private List<Roles> roles = new ArrayList<>();       
 }
