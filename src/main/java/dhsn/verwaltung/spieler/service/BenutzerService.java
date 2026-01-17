2d8cd5670472feda07a5660ff359f9247cb8cf28
lliorlio2301 <jorge2301@outlook.es>
2026-01-17 22:29:48 +0100

    - Implementierung von UserDet..., weil diese  Verbidung mit SpringSecu.. hat, nämlich zu DaoAuthPro.... - UserDe...loadUser..() daten nur per Namen aus der DB abrufen - Bcrypt implementiert für Encryption an die DB - register für neue Benutzer in die DB

--- src/main/java/dhsn/verwaltung/spieler/service/BenutzerService.java
+++ src/main/java/dhsn/verwaltung/spieler/service/BenutzerService.java
@@ -1,5 +1,39 @@
 package dhsn.verwaltung.spieler.service;
 
-public class BenutzerService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
+import dhsn.verwaltung.spieler.model.identity.Benutzer;
+import dhsn.verwaltung.spieler.model.identity.BenutzerDTO;
+import dhsn.verwaltung.spieler.repository.BenutzerRepository;
+
+@Service
+public class BenutzerService implements UserDetailsService {
+
+  private BenutzerRepository benutzerRepository;
+
+  //Verantwortlich das eingegebene Passwort zu encrypten
+  private BCryptPasswordEncoder bEncoder = new BCryptPasswordEncoder(4);
+
+  public BenutzerService (BenutzerRepository benutzerRepository) {
+    this.benutzerRepository = benutzerRepository;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    Benutzer benutzer = benutzerRepository.findByUsername(username);
+
+    if (benutzer==null) {
+      throw new UsernameNotFoundException("User wurde nicht gefunden");
+    }
+    return new BenutzerDTO(benutzer);
+  }
+
+  public Benutzer register(Benutzer benutzer) {
+    benutzer.setPasswort(bEncoder.encode(benutzer.getPasswort()));
+    return benutzerRepository.save(benutzer);
+  }
 }
