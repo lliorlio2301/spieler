12deecb4674d8963322e6fe835767fcf774c90f5
lliorlio2301 <jorge2301@outlook.es>
2026-01-18 16:50:27 +0100

    Role Methode richtig konfiguriert

--- src/main/java/dhsn/verwaltung/spieler/model/identity/BenutzerDTO.java
+++ src/main/java/dhsn/verwaltung/spieler/model/identity/BenutzerDTO.java
@@ -1,9 +1,11 @@
 package dhsn.verwaltung.spieler.model.identity;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 //Diese Klasse ermöglicht eine saubere Übertragung von GUI zu Objekt
@@ -11,6 +13,7 @@ public class BenutzerDTO implements UserDetails {
     
     private String username;
     private String password;
+    private Role role;
 
     public BenutzerDTO(){}
 
@@ -18,6 +21,7 @@ public class BenutzerDTO implements UserDetails {
     public BenutzerDTO(Benutzer benutzer) {
         this.username = benutzer.getUsername();
         this.password = benutzer.getPasswort();
+        this.role = benutzer.getRole();
     }
 
     //Override Methoden notwendig. SprSec verifiziert diese zwanghaft bei jedem User
@@ -28,7 +32,7 @@ public class BenutzerDTO implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return java.util.Collections.emptyList();
+        return List.of(new SimpleGrantedAuthority(this.role.name()));
     }
 
     @Override
