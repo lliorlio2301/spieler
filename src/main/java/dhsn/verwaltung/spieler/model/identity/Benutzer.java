f4ff3d815bcdf2e0b4e56efbca4db06bb834d38f
lliorlio2301 <jorge2301@outlook.es>
2026-01-18 16:50:02 +0100

    Role Feld vereinfacht

--- src/main/java/dhsn/verwaltung/spieler/model/identity/Benutzer.java
+++ src/main/java/dhsn/verwaltung/spieler/model/identity/Benutzer.java
@@ -1,19 +1,13 @@
 package dhsn.verwaltung.spieler.model.identity;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinColumns;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -29,18 +23,8 @@ public class Benutzer {
     
     private String passwort;
 
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinTable( 
-    // Spring configures it automatically but it is configurated, sothat the names of the columns doesn't change if the model's variables change
-        name = "benutzer_roles", 
-        joinColumns = @JoinColumn(name = "benutzer_id", referencedColumnName = "id"),
-        inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
-    )
-    private List<Roles> rolesListe = new ArrayList<>();
-
-    public Benutzer(){
-        rolesListe.add(new Roles());
-    }
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     public String getUsername() {
         return username;
@@ -58,12 +42,12 @@ public class Benutzer {
         this.passwort = passwort;
     }
 
-    public List<Roles> getRolesListe() {
-        return rolesListe;
+    public Role getRole() {
+        return role;
     }
 
-    public void setRolesListe(List<Roles> rolesListe) {
-        this.rolesListe = rolesListe;
+    public void setRole(Role role) {
+        this.role = role;
     }
 
 }
