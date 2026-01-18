db5d2a674f496371d360bf3111e57b58714c5429
lliorlio2301 <jorge2301@outlook.es>
2026-01-18 13:08:53 +0100

    getAuthorities richtig implementiert, wobei eine emptyListe zurückgegeben wird nur fürs Testen

--- src/main/java/dhsn/verwaltung/spieler/model/identity/BenutzerDTO.java
+++ src/main/java/dhsn/verwaltung/spieler/model/identity/BenutzerDTO.java
@@ -10,38 +10,29 @@ import org.springframework.security.core.userdetails.UserDetails;
 public class BenutzerDTO implements UserDetails {
     
     private String username;
-    private String passwort;
+    private String password;
 
     public BenutzerDTO(){}
 
     //Notwendig, um bei Service den Objekt zurückzusenden
     public BenutzerDTO(Benutzer benutzer) {
-        benutzer.setPasswort(passwort);
-        benutzer.setUsername(username);
+        this.username = benutzer.getUsername();
+        this.password = benutzer.getPasswort();
     }
 
+    //Override Methoden notwendig. SprSec verifiziert diese zwanghaft bei jedem User
+    @Override
     public String getUsername() {
         return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public String getPasswort() {
-        return passwort;
-    }
-    public void setPasswort(String passwort) {
-        this.passwort = passwort;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAuthorities'");
+        return java.util.Collections.emptyList();
     }
 
     @Override
     public @Nullable String getPassword() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPassword'");
+        return password;
     }
 }
