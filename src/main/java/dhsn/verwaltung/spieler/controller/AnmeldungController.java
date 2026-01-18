acf6dc9b70fe5d456bbadf6023a35944c9a08147
lliorlio2301 <jorge2301@outlook.es>
2026-01-18 13:04:19 +0100

    Controller für die Anmeldung Funktion erstellt

---
+++ src/main/java/dhsn/verwaltung/spieler/controller/AnmeldungController.java
@@ -0,0 +1,29 @@
+package dhsn.verwaltung.spieler.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("/anmelden")
+public class AnmeldungController {
+
+    @GetMapping
+    public String anmelden(
+        Model model, 
+        @RequestParam(required = false, value = "bye") String logout,
+        @RequestParam(required = false, value = "error") String fehler
+    ) {
+        if (logout!=null) {
+            model.addAttribute("bye", logout);
+        }
+        if (fehler!=null) {
+            model.addAttribute("error", fehler);
+        }
+        return "anmelden";
+    }
+}
