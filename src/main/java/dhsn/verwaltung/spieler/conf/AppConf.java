637628f28b7caad2b66035728738e5bf6a467bf3
lliorlio2301 <jorge2301@outlook.es>
2026-01-19 22:44:59 +0100

    Neue Benutzer fürs Testen

--- src/main/java/dhsn/verwaltung/spieler/conf/AppConf.java
+++ src/main/java/dhsn/verwaltung/spieler/conf/AppConf.java
@@ -1,13 +1,19 @@
 package dhsn.verwaltung.spieler.conf;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import dhsn.verwaltung.spieler.model.domain.Spieler;
+import dhsn.verwaltung.spieler.model.domain.Position;
 import dhsn.verwaltung.spieler.model.identity.Benutzer;
 import dhsn.verwaltung.spieler.model.identity.Role;
 import dhsn.verwaltung.spieler.repository.BenutzerRepository;
+import dhsn.verwaltung.spieler.repository.SpielerRepository;
 
 //Um Methoden als erstes auszuführen
 
@@ -18,6 +24,7 @@ public class AppConf {
     @Bean
     public CommandLineRunner superUserAnlegen(
         BenutzerRepository benutzerRepository,
+        SpielerRepository spielerRepository,
         PasswordEncoder passwordEncoder
      ) {
         // Lambda mit {....} weil es mehrere Methoden innerhalb dieser einzigen Methode gibt   
@@ -28,6 +35,31 @@ public class AppConf {
             benutzer.setRole(Role.ROLE_SUPER);
             benutzerRepository.save(benutzer);
             System.out.printf("Superadmin %n%s %n%s", "Username: SuperAdmin", "welcome");
+
+            //Objekte Generieren für Testen
+            List<Spieler> testSpieler = List.of(
+                new Spieler("jorge.osorio", passwordEncoder.encode("welcome1"), 
+                            "Jorge", "Osorio", 
+                            LocalDate.of(2004, 1, 23), 
+                            Position.MITTELFELD, 4),
+                new Spieler("titan.olli", passwordEncoder.encode("welcome2"), 
+                            "Oliver", "Kahn", 
+                            LocalDate.of(1995, 6, 15), 
+                            Position.TORWART, 1),
+                new Spieler("the.wall", passwordEncoder.encode("welcome3"), 
+                            "Niklas", "Süle", 
+                            LocalDate.of(1997, 9, 3), 
+                            Position.ABWEHRSPIELER, 25),
+                new Spieler("goalgetter", passwordEncoder.encode("welcome4"), 
+                            "Jamal", "Musiala", 
+                            LocalDate.of(2003, 2, 26), 
+                            Position.MITTELSTUERMER, 10),
+                new Spieler("kimmich.j", passwordEncoder.encode("welcome5"), 
+                            "Joshua", "Kimmich", 
+                            LocalDate.of(1995, 2, 8), 
+                            Position.MITTELFELD, 6)
+            );
+        spielerRepository.saveAll(testSpieler);
      };
     }
     
