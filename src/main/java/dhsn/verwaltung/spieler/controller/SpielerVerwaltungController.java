aa9b651883f28eab4e9fd93b9de69bc1ae5c8be5
lliorlio2301 <jorge2301@outlook.es>
2026-01-19 22:47:05 +0100

    - Einfügen der Benutzer Liste bei SuperUser

--- src/main/java/dhsn/verwaltung/spieler/controller/SpielerVerwaltungController.java
+++ src/main/java/dhsn/verwaltung/spieler/controller/SpielerVerwaltungController.java
@@ -1,9 +1,17 @@
 package dhsn.verwaltung.spieler.controller;
 
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import dhsn.verwaltung.spieler.model.identity.Role;
+import dhsn.verwaltung.spieler.repository.BenutzerRepository;
+import dhsn.verwaltung.spieler.service.BenutzerService;
+
 
 
 
@@ -11,6 +19,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/spielerverwaltung")
 public class SpielerVerwaltungController {
+
+    BenutzerService benutzerService;
+
+    
+
+    public SpielerVerwaltungController(BenutzerService bs) {
+    this.benutzerService = bs;
+    }
+
     @GetMapping("/startseite")
     public String getStartseite() {
         return "startseite";
@@ -22,8 +39,14 @@ public class SpielerVerwaltungController {
     }
     
     @GetMapping("/super")
-    public String getSuperUser() {
+    public String getSuperUser(Model model) {
+       
+        model.addAttribute("listeBenutzer", benutzerService
+        .getAllBenutzer()
+        .stream()
+        .filter(b->b.getRole()!=Role.ROLE_SUPER)
+        .collect(Collectors.toList())
+    );
         return "superUser";
-    }
-    
+    }    
 }
