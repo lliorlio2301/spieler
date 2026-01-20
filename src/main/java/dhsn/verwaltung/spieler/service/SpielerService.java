112c1722f19323851f88b331081e877b7864f92d
lliorlio2301 <jorge2301@outlook.es>
2026-01-20 14:42:39 +0100

    Methoden für das Editieren von Spielern

--- src/main/java/dhsn/verwaltung/spieler/service/SpielerService.java
+++ src/main/java/dhsn/verwaltung/spieler/service/SpielerService.java
@@ -2,21 +2,56 @@ package dhsn.verwaltung.spieler.service;
 
 import java.util.List;
 
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import dhsn.verwaltung.spieler.model.domain.Spieler;
 import dhsn.verwaltung.spieler.repository.SpielerRepository;
+import jakarta.transaction.Transactional;
 
 @Service
 public class SpielerService {
-    private SpielerRepository sr;
+  private SpielerRepository sr;
+  private PasswordEncoder passwordEncoder;
 
-    public SpielerService (SpielerRepository sr) {
-        this.sr = sr;
-    }
+  public SpielerService(SpielerRepository sr, PasswordEncoder passwordEncoder) {
+    this.sr = sr;
+    this.passwordEncoder = passwordEncoder;
+  }
 
-    public List<Spieler> getAlleSpieler() {
-        return sr.findAll();
+  public Spieler getSpieler(Long id) {
+    return sr.findById(id).orElseThrow(()->new UsernameNotFoundException("ID ist unbekannt: " +id));
+  }
+
+  public List<Spieler> getAlleSpieler() {
+    return sr.findAll();
+  }
+  
+  
+
+  @Transactional
+  public Spieler speichernEditSpieler(Spieler formularSpieler, Long id) 
+  throws UsernameNotFoundException {
+    Spieler datenBankSpieler = sr.findById(id).orElseThrow(() -> new UsernameNotFoundException("Id nicht bekannt"));
+
+    datenBankSpieler.setUsername(formularSpieler.getUsername());
+    datenBankSpieler.setVorname(formularSpieler.getVorname());
+    datenBankSpieler.setNachname(formularSpieler.getNachname());
+    datenBankSpieler.setGeburtsdatum(formularSpieler.getGeburtsdatum());
+    datenBankSpieler.setPosition(formularSpieler.getPosition());
+    datenBankSpieler.setRueckennummer(formularSpieler.getRueckennummer());
+
+    if (
+      !formularSpieler.getPasswort().isEmpty() &&
+      formularSpieler.getPasswort() != null 
+      && !datenBankSpieler.getPasswort().equals(passwordEncoder.encode(formularSpieler.getPasswort()))
+    ) {
+        
+      datenBankSpieler.setPasswort(passwordEncoder.encode(formularSpieler.getPasswort()));
     }
+    return sr.save(datenBankSpieler);
+  }
 
 }
