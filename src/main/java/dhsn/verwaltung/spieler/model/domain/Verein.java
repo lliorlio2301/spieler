5034c448a3acde935961143127dfa4df28757bab
lliorlio2301 <jorge2301@outlook.es>
2026-01-27 15:17:03 +0100

    Verein Klasse

---
+++ src/main/java/dhsn/verwaltung/spieler/model/domain/Verein.java
@@ -0,0 +1,46 @@
+package dhsn.verwaltung.spieler.model.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import dhsn.verwaltung.spieler.model.identity.Benutzer;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter //Erstellt automatisch Getters und Setters 
+@Entity
+@Table(name = "verein")
+public class Verein {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "verein", fetch = FetchType.LAZY)
+    private List<Spieler> spielerListe = new ArrayList<>();
+
+    @OneToOne
+    @JoinColumn(name = "admin_id", referencedColumnName = "id", unique = true)
+    private Benutzer admin;
+
+    private String name;
+    
+    public Verein(){}
+
+    public Verein(Benutzer admin, String name) {
+        this.admin = admin;
+        this.name = name;
+    }
+}
