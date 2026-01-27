6efb29e6b4f614742e040064bad9dd91f7689e00
lliorlio2301 <jorge2301@outlook.es>
2026-01-27 15:18:20 +0100

    Query, um nur Admins ohne Verein zu bekommen

--- src/main/java/dhsn/verwaltung/spieler/repository/BenutzerRepository.java
+++ src/main/java/dhsn/verwaltung/spieler/repository/BenutzerRepository.java
@@ -1,6 +1,9 @@
 package dhsn.verwaltung.spieler.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import dhsn.verwaltung.spieler.model.identity.Benutzer;
@@ -8,4 +11,8 @@ import dhsn.verwaltung.spieler.model.identity.Benutzer;
 @Repository
 public interface BenutzerRepository extends JpaRepository<Benutzer, Long> {
     Benutzer findByUsername(String username);
+
+    @Query("SELECT b FROM Benutzer b WHERE b.verwalteterVerein IS NULL AND b.role = 'ROLE_ADMIN'")
+    List<Benutzer> findFreieAdmins();
 }
+
