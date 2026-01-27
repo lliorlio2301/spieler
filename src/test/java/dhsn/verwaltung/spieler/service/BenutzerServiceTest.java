22cae7ee59229afc54c28d51524eddb172c5061a
lliorlio2301 <jorge2301@outlook.es>
2026-01-27 21:13:34 +0100

    Sinvoller EInsatz des Test

--- src/test/java/dhsn/verwaltung/spieler/service/BenutzerServiceTest.java
+++ src/test/java/dhsn/verwaltung/spieler/service/BenutzerServiceTest.java
@@ -14,6 +14,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import dhsn.verwaltung.spieler.model.identity.Benutzer;
 import dhsn.verwaltung.spieler.model.identity.Role;
 import dhsn.verwaltung.spieler.repository.BenutzerRepository;
+import dhsn.verwaltung.spieler.repository.SpielerRepository;
+import dhsn.verwaltung.spieler.repository.VereinRepository;
 
 @SpringBootTest
 public class BenutzerServiceTest {
@@ -21,15 +23,25 @@ public class BenutzerServiceTest {
     private BenutzerService benutzerService;
     @Autowired
     private BenutzerRepository benutzerRepository;
+    @Autowired
+    private SpielerRepository spielerRepository;
+    @Autowired
+    private VereinRepository vereinRepository;
 
 
     @Test
     public void testenFreieAdmins(){
 
+        //Die DB muss bereignigt werden von den vorherigen Tests, und zwar alles weil FK vorhanden sind
+        //InBatch schickt direkt den SQLBefehl an die DB. Normales DeleteAll lädt alles und deswegen treten dann Fehler auf
+        spielerRepository.deleteAllInBatch();
+        vereinRepository.deleteAllInBatch();
+        benutzerRepository.deleteAllInBatch();
+
         List<Benutzer> admins = List.of(
-            new Benutzer("test1", "test1", Role.ROLE_ADMIN),
-            new Benutzer("test2", "test1", Role.ROLE_ADMIN),
-            new Benutzer("test3", "test1", Role.ROLE_ADMIN)
+            new Benutzer("testenUser1", "test1", Role.ROLE_ADMIN),
+            new Benutzer("testenUser2", "test1", Role.ROLE_ADMIN),
+            new Benutzer("testenUser3", "test1", Role.ROLE_ADMIN)
         );
 
         benutzerRepository.saveAll(admins);
