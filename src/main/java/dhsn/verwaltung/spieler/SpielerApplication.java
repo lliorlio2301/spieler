d79791ce1e6c97ae6cee4418c4a2254f57b0f923
lliorlio2301 <jorge2301@outlook.es>
2026-01-27 21:14:18 +0100

    Extra Konfigurationen, um Tomcat zu ermöglichen

--- src/main/java/dhsn/verwaltung/spieler/SpielerApplication.java
+++ src/main/java/dhsn/verwaltung/spieler/SpielerApplication.java
@@ -2,10 +2,16 @@ package dhsn.verwaltung.spieler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpielerApplication {
+public class SpielerApplication extends SpringBootServletInitializer {
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpielerApplication.class);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(SpielerApplication.class, args);
 	}
